865473e4e3e6dccdc95ac5e6e99b9bc7b782f3bc
Florian Hämmerle <haemmerle.florian@googlemail.com>
2016-08-01 12:40:06 +0200

    Single Sign-On (#6)

--- public/settings.js
+++ public/settings.js
@@ -2,6 +2,12 @@
   global.iris = global.iris || {};
   global.iris.settings = global.iris.settings || {};
   global.iris.settings.swagger = {
-    specUrl: '/swagger.json'
+    specUrl: 'http://api-iris-dev.lovelysystems.com/swagger.json'
   };
+  global.iris.settings.ssoProviders = [
+    {
+      loginUrl: 'http://aaz-azdev.lovelysystems.com/anmelden',
+      name: 'AZ Dev'
+    }
+  ];
 }(window));
